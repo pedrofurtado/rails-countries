85b26df8e45a7db83009ae4bd6952d6a842cb4cb
Pedro Felipe de Azevedo Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2019-07-18 14:36:12 -0300

    Update engine.rb

--- lib/rails-countries/engine.rb
+++ lib/rails-countries/engine.rb
@@ -1,7 +1,7 @@
 module RailsCountries
   class Engine < ::Rails::Engine
     initializer 'rails-countries.engine' do
-      ActionView::Base.send :include, ::RailsCountries::ActionView::Helpers
+      ::ActionView::Base.send :include, ::RailsCountries::ActionView::Helpers
     end
   end
 end
