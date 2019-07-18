3ec6a70c8ccfec77113ed34ed913112cb0ad2d00
Pedro Felipe de Azevedo Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2019-07-18 13:58:31 -0300

    Update activemodel.rb

--- lib/validates-countries/activemodel.rb
+++ lib/validates-countries/activemodel.rb
@@ -5,7 +5,7 @@ module ActiveModel
   module Validations
     class CountriesAlpha2Validator < EachValidator
       def validate_each(record, attribute, value)
-        record.errors.add(attribute, 'validates_countries.errors.messages.alpha2.invalid', alpha2: value) if value.present? && (value.is_a?(String) || value.is_a?(Symbol)) && !ISO3166::Country.all.map(&:alpha2).include?(value.to_s)
+        record.errors.add(attribute, :countries_alpha2_invalid, alpha2: value) if value.present? && (value.is_a?(String) || value.is_a?(Symbol)) && !ISO3166::Country.all.map(&:alpha2).include?(value.to_s)
       end
     end
 
