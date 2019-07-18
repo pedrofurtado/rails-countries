5b7280d1774e55f19af20d3d0451b5a643968f5d
Pedro Felipe de Azevedo Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2019-07-18 14:58:01 -0300

    Update helpers.rb

--- lib/rails-countries/helpers.rb
+++ lib/rails-countries/helpers.rb
@@ -1,8 +1,12 @@
 module RailsCountries
   module ActionView
     module Helpers
-      def countries_alpha2_options
-        ISO3166::Country.all.map { |country| [country.translation(I18n.locale), country.alpha2] }
+      def countries_alpha2_options(locale = nil)
+        ISO3166::Country.all.map { |country| [country.translation(locale || I18n.locale), country.alpha2] }
+      end
+
+      def i18n_country_name_by_alpha2(alpha2, locale = nil)
+        ISO3166::Country.new(alpha2).translation(locale || I18n.locale)
       end
     end
   end
