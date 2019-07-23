8dac69a23c9aa41db1cbb0bef14ab69e60b9c09f
Pedro Felipe de Azevedo Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2019-07-23 14:28:03 -0300

    Update helpers.rb

--- lib/rails-countries/helpers.rb
+++ lib/rails-countries/helpers.rb
@@ -5,8 +5,16 @@ module RailsCountries
         ISO3166::Country.all.map { |country| [country.translation(locale || I18n.locale), country.alpha2] }
       end
 
+      def countries_alpha3_options(locale = nil)
+        ISO3166::Country.all.map { |country| [country.translation(locale || I18n.locale), country.alpha3] }
+      end
+
       def i18n_country_name_by_alpha2(alpha2, locale = nil)
-        ISO3166::Country.new(alpha2).translation(locale || I18n.locale)
+        ISO3166::Country.new(alpha2)&.translation(locale || I18n.locale)
+      end
+
+      def i18n_country_name_by_alpha3(alpha3, locale = nil)
+        ISO3166::Country.find_country_by_alpha3(alpha3)&.translation(locale || I18n.locale)
       end
     end
   end
