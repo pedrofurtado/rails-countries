34999ae5469367ff498ec56873ea04b3a8a8a5fd
Pedro Felipe de Azevedo Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2019-07-23 14:15:09 -0300

    alpha3 validator

--- lib/rails-countries/validators.rb
+++ lib/rails-countries/validators.rb
@@ -8,9 +8,19 @@ module ActiveModel
       end
     end
 
+    class CountriesAlpha3Validator < EachValidator
+      def validate_each(record, attribute, value)
+        record.errors.add(attribute, :countries_alpha3_invalid, alpha3: value) if value.present? && (value.is_a?(String) || value.is_a?(Symbol)) && !ISO3166::Country.all.map(&:alpha3).include?(value.to_s)
+      end
+    end
+
     module HelperMethods
       def validates_countries_alpha2_of(*attributes)
         validates_with CountriesAlpha2Validator, _merge_attributes(attributes)
+      end
+      
+      def validates_countries_alpha3_of(*attributes)
+        validates_with CountriesAlpha3Validator, _merge_attributes(attributes)
       end
     end
   end
